b436bef9067e53666193e98d852dba93c71c7016
Danik <danilalappo6@gmail.com>
2022-06-22 11:20:14 +0300

    first commit

---
+++ Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib> // для system
+using namespace std;
+
+
+int main()
+{
+    int k = 0;
+    cout << "Hello, world!" << endl;
+    cout << "4";
+    system("pause"); // Только для тех, у кого MS Visual Studio
+    return 0;
+}
