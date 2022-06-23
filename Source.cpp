e2086d1893675249b2952b61e2adb71648c616a4
Danik <danilalappo6@gmail.com>
2022-06-23 11:15:34 +0300

    2  commit

--- Source.cpp
+++ Source.cpp
@@ -7,7 +7,7 @@ int main()
 {
     int k = 0;
     cout << "Hello, world!" << endl;
-    cout << "5";
+    cout << "13";
     system("pause"); // Только для тех, у кого MS Visual Studio
     return 0;
 }
